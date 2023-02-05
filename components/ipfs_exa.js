e8dc57a8033bf5970a7680b9c7ed2bf8a3058b8b
FurkanSezal <fsezal46@gmail.com>
2023-02-05 05:12:49 +0300

    version i dont know

--- components/ipfs_exa.js
+++ components/ipfs_exa.js
@@ -2,6 +2,9 @@ import Link from "next/link"
 import { pack, packToBlob } from "ipfs-car/pack"
 import { MemoryBlockStore } from "ipfs-car/blockstore/memory" // You can also use the `level-blockstore` module
 import React, { useState, useEffect } from "react"
+import { CID } from "multiformats/cid"
+import * as json from "multiformats/codecs/json"
+import { sha256 } from "multiformats/hashes/sha2"
 
 export default function Hashh() {
     const [selectedFile, setSelectedFile] = useState()
@@ -11,6 +14,7 @@ export default function Hashh() {
         console.log(event.target.files[0])
     }
     useEffect(() => {}, [])
+
     async function handleClick() {
         const { root, out } = await pack({
             input: [selectedFile],
@@ -23,8 +27,12 @@ export default function Hashh() {
         for await (const part of out) {
             carParts.push(part)
         }
-        console.log(root)
-        console.log(carParts)
+
+        const bytes = json.encode(carParts)
+
+        const hash = await sha256.digest(bytes)
+        const cid = CID.create(1, json.code, hash)
+        console.log(cid.toString())
     }
     return (
         <div>
